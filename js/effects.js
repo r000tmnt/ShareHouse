4b0f3e46c22bab9cb4bb3750194eef85c6a870e4
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2020-08-21 15:03:00 +0800

    修改部分Style，測試開啟隱藏dialog成功

--- js/effects.js
+++ js/effects.js
@@ -2,3 +2,7 @@ $(document).ready(function(){
     $(".JS").hide().fadeIn(1300);
     $(".NQ").hide().fadeIn(2200);
 });
+
+function popUp(){
+    document.getElementById('dialog').style.display="block";
+}
