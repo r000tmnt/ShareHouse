f07162a758886b3a0d4e40e2e5ce7dd098e56abc
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2020-08-25 14:02:16 +0800

    改用Jquery寫開關功能

--- js/effects.js
+++ js/effects.js
@@ -1,14 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function(){ //導入頁淡入特效
     $(".JS").hide().fadeIn(1300);
     $(".NQ").hide().fadeIn(2200);
+
+    $(".btn").click(function(){ //開啟指定的房間資料頁面
+        var btn= $(".btn");
+
+        if(btn.hasClass('A')){
+                $("#dialog").show(),
+                $("div.A").show();
+               }
+               else if(btn.hasClass('B')){
+                $("#dialog").show(),
+                $("div.B").show();
+               }
+               else if(btn.hasClass('C')){
+                $("#dialog").show(),
+                $("div.C").show();    
+               }
+               else if(btn.hasClass('D')){
+                $("#dialog").show(),
+                $("div.D").show();
+               }
+
+    });
+
+    $(".close").click(function(){ //關閉
+        $("#dialog").hide();
+    });
 });
-
-function popUp(){
-    var box= document.getElementById('dialog');
-    box.style.display="block";
-}
-
-function close(){
-    var close= document.getElementsByClassName('dialog');
-    close.style.display= "none";
-}
