797f5ec6732aeb260fa8e115ca1c7877fb6a35b1
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2020-08-25 16:34:23 +0800

    改用if else if 搭配 $(this).is 來判斷當前按鈕的class來決定打開哪個div

--- js/effects.js
+++ js/effects.js
@@ -3,28 +3,22 @@ $(document).ready(function(){ //導入頁淡入特效
     $(".NQ").hide().fadeIn(2200);
 
     $(".btn").click(function(){ //開啟指定的房間資料頁面
-        var btn= $(".btn");
-
-        if(btn.hasClass('A')){
-                $("#dialog").show(),
-                $("div.A").show();
-               }
-               else if(btn.hasClass('B')){
-                $("#dialog").show(),
-                $("div.B").show();
-               }
-               else if(btn.hasClass('C')){
-                $("#dialog").show(),
-                $("div.C").show();    
-               }
-               else if(btn.hasClass('D')){
-                $("#dialog").show(),
-                $("div.D").show();
-               }
-
+        
+        if($(this).is('.A')){
+            $("#dialog, .room_a").show();
+        }
+        else if($(this).is('.B')){
+            $("#dialog, .room_b").show();
+        }
+        else if($(this).is('.C')){
+            $("#dialog, .room_c").show();
+        }
+        else if($(this).is('.D')){
+            $("#dialog, .room_d").show();
+        }
     });
 
     $(".close").click(function(){ //關閉
-        $("#dialog").hide();
+        $("#dialog, .room").hide();
     });
 });
